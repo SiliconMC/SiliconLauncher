5d9e4aebfa9b8d61d3a90fc8894e0c94b57132bd
jack <63988891+jwhazy@users.noreply.github.com>
2021-12-13 22:01:10 +1300

    New Login interfaces

--- SiliconLauncher/Accounts/Login.xaml.cs
+++ SiliconLauncher/Accounts/Login.xaml.cs
@@ -1,6 +1,14 @@
-﻿using System.IO;
-using System.Threading;
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+using Newtonsoft.Json;
+using SiliconLauncher.Helpers;
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
 using System.Windows;
+using System.Windows.Media.Imaging;
 
 namespace SiliconLauncher
 {
@@ -8,7 +16,7 @@ namespace SiliconLauncher
     {
         public Login()
         {
-            Thread.Sleep(1500);
+
             if (File.Exists(Globals.SiliconData + "\\Silicon\\account.json"))
             {
                 MainWindow mainWin = new MainWindow();
@@ -18,22 +26,34 @@ namespace SiliconLauncher
 
             InitializeComponent();
             Main = this;
+
             Loaded += (s, e) =>
             {
                 MinimizeButton.IsEnabled = true;
                 MaximizeRestoreButton.IsEnabled = false;
                 CloseButton.IsEnabled = true;
-            };
 
+            };
         }
 
-        internal static Login Main;
-
-        private void MojangLoginButton_Click(object sender, RoutedEventArgs e)
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
         {
-            MojangLogin mojangLogin = new MojangLogin();
-            mojangLogin.Show();
-            Close();
+            var username = EmailBox.Text;
+            var password = PassBox.Password;
+
+            if (String.IsNullOrEmpty(username) || String.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Fill in your Mojang credentials.");
+                return;
+            }
+
+            if (MojangAccounts.Authenticate(username, password))
+            {
+                MainWindow mainWin = new MainWindow();
+                mainWin.Show();
+                Close();
+            }
+
         }
 
         private void MicrosoftLoginButton_Click(object sender, RoutedEventArgs e)
@@ -42,5 +62,12 @@ namespace SiliconLauncher
             microsoftLogin.Show();
             Close();
         }
+
+        internal static Login Main;
     }
 }
+
+
+
+
+
--- SiliconLauncher/Accounts/MicrosoftLogin.xaml.cs
+++ SiliconLauncher/Accounts/MicrosoftLogin.xaml.cs
@@ -1,8 +1,16 @@
-﻿using System;
+﻿
+
+using Microsoft.Toolkit.Uwp.Notifications;
+using Newtonsoft.Json;
+using SiliconLauncher.Helpers;
+using System;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
-using System.Threading;
+using System.IO.Compression;
+using System.Net;
 using System.Windows;
+using System.Windows.Media.Imaging;
 
 namespace SiliconLauncher
 {
@@ -10,8 +18,7 @@ namespace SiliconLauncher
     {
         public MicrosoftLogin()
         {
-            Directory.CreateDirectory(Globals.SiliconData + "\\Silicon");
-            Thread.Sleep(1500);
+
             if (File.Exists(Globals.SiliconData + "\\Silicon\\account.json"))
             {
                 MainWindow mainWin = new MainWindow();
@@ -19,17 +26,15 @@ namespace SiliconLauncher
                 Close();
             }
 
-
-
             InitializeComponent();
-
-
             Main = this;
+
             Loaded += (s, e) =>
             {
                 MinimizeButton.IsEnabled = true;
                 MaximizeRestoreButton.IsEnabled = false;
                 CloseButton.IsEnabled = true;
+
                 Process.Start("https://login.live.com/oauth20_authorize.srf ?client_id=e562da4a-5002-4efd-8667-e74618f2d85d&response_type=code&redirect_uri=https://silicon-auth.jacksta.workers.dev/auth/microsoft&scope=XboxLive.signin offline_access");
             };
 
@@ -38,7 +43,7 @@ namespace SiliconLauncher
 
         private void LoginButton_Click(object sender, RoutedEventArgs e)
         {
-            var token = TokenBox.Text;
+            var token = TokenBox.Password;
 
             if (String.IsNullOrEmpty(token))
             {
@@ -47,10 +52,16 @@ namespace SiliconLauncher
             }
 
             MicrosoftAccounts.AuthenticateXbl(token);
-
         }
 
         internal static MicrosoftLogin Main;
 
+        private void ReturnButton_Click(object sender, RoutedEventArgs e)
+        {
+            Login login = new Login();
+            login.Show();
+            Close();
+        }
     }
 }
+
